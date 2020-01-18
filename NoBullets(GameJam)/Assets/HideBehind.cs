e6bde93441ddaafc1d866907cafc2059e77b504b
K-Krup <krupennyi@users.noreply.github.com>
2020-01-18 17:01:32 +0100

    GirlScript

--- NoBullets(GameJam)/Assets/HideBehind.cs
+++ NoBullets(GameJam)/Assets/HideBehind.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class HideBehind : MonoBehaviour
 {
 	bool inReach;
+	GameObject hideObject;
 
     void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.gameObject.tag == "Hideble")
+		{
 			inReach = true;
+			hideObject = col.gameObject;
+		}
 	}
 	void OnTriggerExit2D(Collider2D col)
 	{
 		if (col.gameObject.tag == "Hideble")
+		{
 			inReach = false;
+			hideObject = null;
+		}
 	}
 
 	void Update()
@@ -27,6 +34,9 @@ public class HideBehind : MonoBehaviour
 					GetComponent<SpriteRenderer>().color = Color.grey;
 					GetComponent<GirlState>().state = GirlState.State.Hide;
 					GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+					MoveToHide();
+
 				}
 
 				else if (GetComponent<GirlState>().state == GirlState.State.Hide)
@@ -38,5 +48,24 @@ public class HideBehind : MonoBehaviour
 
 		}
 	}
-	
+
+	void MoveToHide()
+	{
+		if (hideObject.transform.childCount > 1)
+		{
+			if (Vector2.Distance(hideObject.transform.GetChild(0).transform.position, transform.position) <=
+							Vector2.Distance(hideObject.transform.GetChild(1).transform.position, transform.position))
+			{
+				transform.position = hideObject.transform.GetChild(0).transform.position;
+			}
+			else
+			{
+				transform.position = hideObject.transform.GetChild(1).transform.position;
+			}
+		}
+		else
+		{
+			transform.position = hideObject.transform.GetChild(0).transform.position;
+		}
+	}
 }
