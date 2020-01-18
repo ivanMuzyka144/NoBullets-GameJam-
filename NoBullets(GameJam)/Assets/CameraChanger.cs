ac1dec54d66b64416b0836843c6c0396a4b8b888
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-01-18 23:40:19 +0100

    Add trigger zones

---
+++ NoBullets(GameJam)/Assets/CameraChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public GameObject myCamera;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        myCamera.GetComponent<Animator>().SetBool("ShouldMove", !(myCamera.GetComponent<Animator>().GetBool("ShouldMove")));
+        
+    }
+    
+}
